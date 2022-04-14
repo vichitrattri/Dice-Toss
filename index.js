bc5caa61d0953a1ab26c390d747d84a3342c558a
Vichitra Attri <mevichitra@gmail.com>
2022-04-14 20:14:30 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var num1 = Math.floor(Math.random() * 6) + 1;
+var num2 = Math.floor(Math.random() * 6) + 1;
+
+document
+  .querySelector("img.img1")
+  .setAttribute("src", "./images/dice" + num1 + ".png");
+document
+  .querySelector("img.img2")
+  .setAttribute("src", "./images/dice" + num2 + ".png");
+
+if (num1 > num2) {
+  document.querySelector("h1").textContent = "🎖 Player 1 Wins";
+} else if (num1 === num2) {
+  document.querySelector("h1").textContent = "It's a Draw";
+} else {
+  document.querySelector("h1").textContent = "🎖 Player 2 Wins";
+}
